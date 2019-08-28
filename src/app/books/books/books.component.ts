39089727b98b531e53360e6be5b0364974031cf2
Christian <ch.m.dittrich@gmail.com>
2019-08-28 15:13:33 +0200

    Hinzufügen de rNavigate Component

--- src/app/books/books/books.component.ts
+++ src/app/books/books/books.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent implements OnInit {
+  bookTitle: string;
+  price: number
 
-  constructor() { }
+  constructor() {
+    this.bookTitle = '"Hunde für Anfänger"';
+    this.price = 23;
+  }
 
   ngOnInit() {
   }
