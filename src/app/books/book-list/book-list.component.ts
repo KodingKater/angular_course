d0ec84c3c2e5c88856751ca0a2cffcaa14e86c8a
Christian <ch.m.dittrich@gmail.com>
2019-08-29 16:09:21 +0200

    Hinzufügen von Routes

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BookDataService} from '../book-data.service';
 import {Book} from '../book';
 import {getErrorMessage} from 'codelyzer/templateAccessibilityElementsContentRule';
+import {Subscription} from 'rxjs';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -9,17 +10,25 @@ import {getErrorMessage} from 'codelyzer/templateAccessibilityElementsContentRul
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-   bookListe: Book[];
-   errorMessage: string;
+  bookListe: Book[];
+  getSubscriptions: Subscription[];
+  errorMessage: string;
 
   constructor(private bookData: BookDataService) {
-    bookData.getBooks().subscribe((nextBookList) =>
-      this.bookListe = nextBookList, error => this.errorMessage = error.message);
+    this.getSubscriptions = [];
   }
 
   ngOnInit() {
+    this.getSubscriptions.push(
+      this.bookData.getBooks().subscribe(
+        (nextBookList) => this.bookListe = nextBookList,
+        error => this.errorMessage = error.message));
+  }
+
+  ngOnDestroy() {
+    this.getSubscriptions.forEach(x => x.unsubscribe());
   }
 
 }
